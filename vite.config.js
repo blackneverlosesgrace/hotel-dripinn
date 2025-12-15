a0e1bca66fb477c88e11027a5416888fda6a52ec
blackneverlosesgrace <swati.infome@gmail.com>
2025-12-15 07:40:33 +0530

    Configure GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/hotel-dripinn/',
   server: {
     allowedHosts: ['testaceous-insistently-terrilyn.ngrok-free.dev'],
     proxy: {
